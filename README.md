f345c765599da7d833e58f83a6cb08fd38b797af
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-02-04 20:42:34 -0500

    updates Feb 4

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Project-1
 First Project for Bootcamp
 
+Krave - Food App
 
-## Hey Guys lets make a project
-Here is my idea: Tinder for cats...think about it.
+App allows user to make decisions about whether to cook at home or to find food options outside the home. With a simple search of a cuisine or specific dish + zipcode, user will be presented with a instructional video for making searched food or local options where the food may be purchased.
+
+This app utilizes:
+Google Places API
+Youtube API
+Materialize
+Google Fonts
+
+Database: Firebase
+
+Deployment: Firebase
+
+Authors: Kiran, Alex, Katy, Ethan 
+
+
--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
-// https://developers.google.com/youtube/iframe_api_reference
-
-// https://developers.google.com/youtube/v3/docs/search#resource
 function loadPlayer() {
     window.onYouTubePlayerAPIReady = function () {
         onYouTubePlayer();
     };
 }
 
-//Set then firebase configuration
+//Set the firebase configuration
 
 var config = {
     apiKey: "AIzaSyAyTbXmD3Afa1S6ZUFFXm-vnkSegSEFNcQ",
@@ -20,7 +17,7 @@ var config = {
 firebase.initializeApp(config);
 
 // Create a variable to reference the database
-var database = firebase.database();
+var database = firebase.database()
 
 $(document).ready(function () {
 
@@ -29,100 +26,120 @@ $(document).ready(function () {
         console.log("Search Term: " + childSnapshot.val().search);
         console.log("Zip: " + childSnapshot.val().zip);
 
+
+        // Search results are appended to the table
         $("#resultsTable").append(
             `<tr><th scope="row">${childSnapshot.val().search}</th>`
+            // `<tr><th scope="row">${childSnapshot.val().zip}</th>`
         );
+        // only 5 searches are presented
+        // $("table > tbody > tr").hide().slice(0, 5).show();
+        $('table tr:gt(5)').hide();
+
+
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
 
-    $("#submit").on("click", function (event) {
-        event.preventDefault();
-        var search = $("#search").val().trim();
-        var zip = $("#zip").val().trim();
-        // create a temporary object for holding the new train data
-        var recentSearch = {
-            search: search,
-            zip: zip
-        };
-
-        console.log(`Hey ${recentSearch}`)
-        // upload the new train data to the database
-        database.ref("/recipes").push(recentSearch);
-        // console log the values that were just pushed to the database
-        console.log(recentSearch.search);
-        console.log(recentSearch.zip);
-        // clear the form values after values have been stored
-        wait()
-        // create a firebase event for adding the data from the new trains and then populating them in the DOM.
-    });
-
-
-
-
-
-    // function to load videos
-    function wait() {
-        $("#recipes").empty();
-        loadPlayer();
-        var input = $("#search").val().trim() + " recipe";
-        console.log(input);
-        var apiKey = "&key=AIzaSyC-eggb7gTlK5ThaxuAyyXs6jqXZ92fXk0";
-        var queryURL = `https://www.googleapis.com/youtube/v3/search?q=${input}&${apiKey}&maxResults=1&part=snippet`;
-        console.log(queryURL);
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            // function that executes to display videos on the page
-            onYouTubeIframeAPIReady(response.items[0].id.videoId, "recipes")
-        })
-    }
-
-    var player;
-
-    //THis function creates a youtube object
-    function onYouTubeIframeAPIReady(vidId, spot) {
-        player = new YT.Player(spot, {
-            height: '390',
-            width: '640',
-            videoId: vidId,
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
-            }
-        });
-
-    }
-
-    // 4. The API will call this function when the video player is ready.
-    function onPlayerReady(event) {
-        event.target.playVideo();
-    }
-
-    // 5. The API calls this function when the player's state changes.
-    //    The function indicates that when playing a video (state=1),
-    //    the player should play for six seconds and then stop.
-    var done = false;
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-            setTimeout(stopVideo, 1000);
-            done = true;
-        }
-    }
-    function stopVideo() {
-        player.stopVideo();
-        resetForm();
-    }
-
-
-    player.destroy();
-    //Resets the input once the player is loaded
-
-    function resetForm() {
-        document.getElementById("search").value = "";
-        document.getElementById("zip").value = "";
-    }
 
 });
+
+$("#submit").on("click", function (event) {
+
+    event.preventDefault();
+    var search = $("#search").val().trim();
+    var zip = $("#zip").val().trim();
+    console.log($("#zip"))
+    // create a temporary object for holding the new search data
+    var recentSearch = {
+        search: search,
+        zip: zip
+    };
+
+    console.log(`Hey ${recentSearch}`)
+    // upload the new search data to the database
+    database.ref("/recipes").push(recentSearch);
+    // console log the values that were just pushed to the database
+    console.log(recentSearch.search);
+    console.log(recentSearch.zip);
+    // call function wait() to load videos based on the search term
+    wait();
+
+});
+
+// function to load videos
+function wait() {
+
+    $("#recipes").empty();
+    loadPlayer();
+    var input = $("#search").val().trim() + " recipe";
+    console.log(input);
+    var apiKey = "&key=AIzaSyC-eggb7gTlK5ThaxuAyyXs6jqXZ92fXk0";
+    var queryURL = `https://www.googleapis.com/youtube/v3/search?q=${input}${apiKey}&maxResults=1&part=snippet`;
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // function that executes to display videos on the page
+        onYouTubeIframeAPIReady(response.items[0].id.videoId, "recipes")
+    })
+
+    // Clears the iframe and resets the inputs
+    player.destroy();
+    resetForm();
+
+}
+
+var player;
+
+//This function creates a youtube object
+function onYouTubeIframeAPIReady(vidId, vidReady) {
+    player = new YT.Player(vidReady, {
+        height: '390',
+        width: '640',
+        videoId: vidId,
+        events: {
+            'onReady': onPlayerReady,
+            // 'onStateChange': onPlayerStateChange
+        }
+    });
+    console.log(vidId);
+}
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    // onPlayerStateChange();
+    event.target.playVideo();
+    stopVideo();
+}
+
+// The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for one second and then stop.
+
+// var done = false;
+// function onPlayerStateChange(event) {
+//     if (event.data == YT.PlayerState.PLAYING && !done) {
+//         setTimeout(stopVideo, 1000);
+//         done = true;
+//     }
+
+
+// }
+
+function stopVideo() {
+
+    player.stopVideo();
+    //Resets the input once the player is loaded
+    resetForm();
+}
+
+// clear the form values after values have been stored
+function resetForm() {
+    document.getElementById("search").value = "";
+    document.getElementById("zip").value = "";
+
+}
