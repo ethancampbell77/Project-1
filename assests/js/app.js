d947944528c8fb57d2119fd81fd9ba1197d48852
Kiran <kkotturu@gmail.com>
2019-02-05 20:30:22 -0500

    updated

--- assests/js/app.js
+++ assests/js/app.js
@@ -4,6 +4,21 @@ function loadPlayer() {
     };
 }
 
+// this function takes the zip code and changes it to longitutde and lattitdue for resurant search
+
+function lonlat() {
+    var input = $("#zip").val().trim();
+
+    var xhr = $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + input + "&key=AIzaSyA6N-1it5aWPiccey5v0jP30BI9HAlZVME");
+
+    xhr.done(function (data) {
+        latitude = data.results[0].geometry.location.lat;
+        longitude = data.results[0].geometry.location.lng;
+
+    });
+
+}
+
 //Set the firebase configuration
 
 var config = {
@@ -17,7 +32,7 @@ var config = {
 firebase.initializeApp(config);
 
 // Create a variable to reference the database
-var database = firebase.database()
+var database = firebase.database();
 
 $(document).ready(function () {
 
@@ -26,15 +41,21 @@ $(document).ready(function () {
         console.log("Search Term: " + childSnapshot.val().search);
         console.log("Zip: " + childSnapshot.val().zip);
 
+        // var vidLink = "https://www.youtube.com/watch?v=" & vidId;
+
 
         // Search results are appended to the table
         $("#resultsTable").append(
-            `<tr><th scope="row">${childSnapshot.val().search}</th>`
-                `<tr><th scope="row">${childSnapshot.val().zip}</th>`
+            // `<tr><td>${childSnapshot.val().search}</td></tr>`
+            //     `<tr><td>${childSnapshot.val().zip}</td></tr>`
+
+            "<tr><td id='recentSearch'> " + childSnapshot.val().search +
+            "</td><td id='zip'> " + childSnapshot.val().zip + "</td></tr>"
+
         );
-        // only 5 searches are presented
-        // $("table > tbody > tr").hide().slice(0, 5).show();
+        // First 5 searches are presented in the table
         $('table tr:gt(5)').hide();
+
 
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
@@ -48,13 +69,13 @@ $("#submit").on("click", function (event) {
     event.preventDefault();
     var search = $("#search").val().trim();
     var zip = $("#zip").val().trim();
+
     // create a temporary object for holding the new search data
     var recentSearch = {
         search: search,
         zip: zip
     };
 
-    console.log(`Hey ${recentSearch}`)
     // upload the new search data to the database
     database.ref("/recipes").push(recentSearch);
     // console log the values that were just pushed to the database
@@ -62,6 +83,15 @@ $("#submit").on("click", function (event) {
     console.log(recentSearch.zip);
     // call function wait() to load videos based on the search term
     wait();
+
+    lngLat();
+    console.log(zip);
+    console.log("alex" + latitude)
+    console.log("alex" + longitude)
+
+    restaurant();
+    console.log(latitude)
+    console.log(longitude)
 
 });
 
@@ -82,7 +112,7 @@ function wait() {
     }).then(function (response) {
         console.log(response);
         // function that executes to display videos on the page
-        onYouTubeIframeAPIReady(response.items[0].id.videoId, "recipes")
+        onYouTubeIframeAPIReady(response.items[0].id.videoId, "recipes");
     })
 
     // Clears the iframe and resets the inputs
@@ -114,20 +144,6 @@ function onPlayerReady(event) {
     stopVideo();
 }
 
-// The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for one second and then stop.
-
-// var done = false;
-// function onPlayerStateChange(event) {
-//     if (event.data == YT.PlayerState.PLAYING && !done) {
-//         setTimeout(stopVideo, 1000);
-//         done = true;
-//     }
-
-
-// }
-
 function stopVideo() {
 
     player.stopVideo();
@@ -140,4 +156,40 @@ function resetForm() {
     document.getElementById("search").value = "";
     document.getElementById("zip").value = "";
 
+}
+
+// calls google maps for the location coordinates
+function lngLat() {
+
+    zip = $("#zip").val()
+    $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:" + zip + "&sensor=false&key=AIzaSyA6N-1it5aWPiccey5v0jP30BI9HAlZVME",
+        method: "GET",
+    }).then(function (data) {
+        latitude = data.results[0].geometry.location.lat;
+        longitude = data.results[0].geometry.location.lng;
+
+    }
+    )
+};
+
+function restaurant() {
+
+    // call google api to search for resturants
+    $("#restaurants").empty();
+    var input = $("#search").val().trim();
+    console.log(input);
+    var apiKey = "&key=AIzaSyC-eggb7gTlK5ThaxuAyyXs6jqXZ92fXk0";
+    var queryURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=restaurant&keyword=" + input + apiKey + "&location=" + latitude + "," + longitude + "&radius=2000"
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        // function that executes to display videos on the page
+        // $("#restaurants").text(response[0].name)
+
+        lngLat(response[0].name, "restaurants")
+    })
 }
