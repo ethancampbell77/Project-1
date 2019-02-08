14782a8898c9535b617cdccc81524ac31c5b44d8
CW Wolf <44934371+ethancampbell77@users.noreply.github.com>
2019-02-07 22:27:07 -0500

    hoping this works

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
+
 function loadPlayer() {
     window.onYouTubePlayerAPIReady = function () {
         onYouTubePlayer();
     };
 }
-
 //Set the firebase configuration
 
 var config = {
-    apiKey: "AIzaSyAyTbXmD3Afa1S6ZUFFXm-vnkSegSEFNcQ",
-    authDomain: "krave-project-1.firebaseapp.com",
-    databaseURL: "https://krave-project-1.firebaseio.com",
-    projectId: "krave-project-1",
-    storageBucket: "krave-project-1.appspot.com",
-    messagingSenderId: "833313290365"
+    apiKey: "AIzaSyDmq8i67EXllR8fCRDjnkyLxmxHcHv-y5Y",
+    authDomain: "krave-project1.firebaseapp.com",
+    databaseURL: "https://krave-project1.firebaseio.com",
+    projectId: "krave-project1",
+    storageBucket: "krave-project1.appspot.com",
+    messagingSenderId: "667959221768"
 };
 firebase.initializeApp(config);
 
 // Create a variable to reference the database
-var database = firebase.database()
+var database = firebase.database();
 
 $(document).ready(function () {
 
@@ -26,42 +26,25 @@ $(document).ready(function () {
         console.log("Search Term: " + childSnapshot.val().search);
         console.log("Zip: " + childSnapshot.val().zip);
 
-
         // Search results are appended to the table
-        $("#resultsTable").append(
-            `<tr><th scope="row">${childSnapshot.val().search}</th>`
-            // `<tr><th scope="row">${childSnapshot.val().zip}</th>`
+        $("#resultsTable").append(`<tr><td id='recentSearch'><a href="${childSnapshot.val().videoId}" target="_blank">${childSnapshot.val().search}
+        </a></td><td id='zip'>${childSnapshot.val().zip}</td></tr>`
         );
-        // only 5 searches are presented
-        // $("table > tbody > tr").hide().slice(0, 5).show();
+        // First 5 searches are presented in the table
         $('table tr:gt(5)').hide();
-
 
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
-
-
 });
+
 
 $("#submit").on("click", function (event) {
 
     event.preventDefault();
     var search = $("#search").val().trim();
     var zip = $("#zip").val().trim();
-    console.log($("#zip"))
-    // create a temporary object for holding the new search data
-    var recentSearch = {
-        search: search,
-        zip: zip
-    };
 
-    console.log(`Hey ${recentSearch}`)
-    // upload the new search data to the database
-    database.ref("/recipes").push(recentSearch);
-    // console log the values that were just pushed to the database
-    console.log(recentSearch.search);
-    console.log(recentSearch.zip);
     // call function wait() to load videos based on the search term
     wait();
 
@@ -74,6 +57,7 @@ function wait() {
     loadPlayer();
     var input = $("#search").val().trim() + " recipe";
     console.log(input);
+
     var apiKey = "&key=AIzaSyC-eggb7gTlK5ThaxuAyyXs6jqXZ92fXk0";
     var queryURL = `https://www.googleapis.com/youtube/v3/search?q=${input}${apiKey}&maxResults=1&part=snippet`;
     console.log(queryURL);
@@ -84,16 +68,22 @@ function wait() {
     }).then(function (response) {
         console.log(response);
         // function that executes to display videos on the page
-        onYouTubeIframeAPIReady(response.items[0].id.videoId, "recipes")
+        console.log(response.items[0])
+        onYouTubeIframeAPIReady(response.items[0].id.videoId, "recipes");
+        lngLat()
+
+        let objvideo = {
+            search: $("#search").val().trim(),
+            zip: $("#zip").val().trim(),
+            videoId: "https://www.youtube.com/watch?v=" + response.items[0].id.videoId
+        }
+
+        database.ref("/recipes").push(objvideo);
     })
 
-    // Clears the iframe and resets the inputs
+    // Clears the iframe 
     player.destroy();
-    resetForm();
-
 }
-
-var player;
 
 //This function creates a youtube object
 function onYouTubeIframeAPIReady(vidId, vidReady) {
@@ -103,7 +93,6 @@ function onYouTubeIframeAPIReady(vidId, vidReady) {
         videoId: vidId,
         events: {
             'onReady': onPlayerReady,
-            // 'onStateChange': onPlayerStateChange
         }
     });
     console.log(vidId);
@@ -116,30 +105,56 @@ function onPlayerReady(event) {
     stopVideo();
 }
 
-// The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for one second and then stop.
-
-// var done = false;
-// function onPlayerStateChange(event) {
-//     if (event.data == YT.PlayerState.PLAYING && !done) {
-//         setTimeout(stopVideo, 1000);
-//         done = true;
-//     }
-
-
-// }
-
 function stopVideo() {
-
     player.stopVideo();
     //Resets the input once the player is loaded
     resetForm();
 }
 
-// clear the form values after values have been stored
+// clears the form values after added to the table
 function resetForm() {
     document.getElementById("search").value = "";
     document.getElementById("zip").value = "";
+}
 
+// The below functions are used to populate the restaurants div
+// calls google maps for the location coordinates
+function lngLat() {
+    zip = $("#zip").val()
+    $.ajax({
+        url: "https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:" + zip + "&sensor=false&key=AIzaSyA6N-1it5aWPiccey5v0jP30BI9HAlZVME",
+        method: "GET",
+    }).then(function (data) {
+        latitude = data.results[0].geometry.location.lat.toFixed(2);
+        longitude = data.results[0].geometry.location.lng.toFixed(2);
+        restaurant(latitude, longitude);
+    })
+};
+
+function restaurant(lat, lng) {
+    // call google api to search for resturants
+    var input = $("#search").val().trim();
+    $("#restaurants").empty();
+    console.log(input);
+    // var apiKey = "&key=AIzaSyAu8NcOLpw_ueSUa6w_oE8_rv76uOln-EA";
+    var qrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=restaurant&keyword=" + input + "&key=AIzaSyAu8NcOLpw_ueSUa6w_oE8_rv76uOln-EA&location=" + lat + "," + lng + "&radius=10000";
+    console.log(qrl);
+
+    $.ajax({
+        url: qrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log(response.results[0].name);
+        // function that executes to display videos on the page
+        if (response.results.length) {
+            response.results.forEach((place, i) => {
+                if (i < 5) {
+                    $("#restaurants").append(`<p class="pname">${place.name}</p><p>${place.vicinity}</p>`);
+                } else {
+                    return;
+                }
+            });
+        }
+    })
 }
